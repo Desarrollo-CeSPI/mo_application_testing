4772e5c06d51b579e870758603705e77392d0151
Christian Rodriguez <chrodriguez@gmail.com>
2017-04-12 09:39:32 -0300

    eliminamos de los servers de testing las estadisticas y monitoreo

--- libraries/helper.rb
+++ libraries/helper.rb
@@ -1,8 +1,8 @@
 def php_testing_applications
   mo_testing_apps_from_databag 'mo_application_testing' do |data|
     mo_application_deploy data, :mo_application_php
-    mo_application_php_monitoring data
-    mo_application_php_statistics data
+#    mo_application_php_monitoring data
+#    mo_application_php_statistics data
   end
 end
 
@@ -11,8 +11,8 @@ def ruby_testing_applications
     data['dotenv'] ||= {'PATH' => '$PATH', 'HOME' => '$HOME', 'SECRET_KEY_BASE' => SecureRandom.hex(64),'RAILS_ENV' => 'production'}
     data['services'] ||= {'*' => nil }
     ruby_application data
-    mo_application_ruby_monitoring data
-    mo_application_ruby_statistics data
+#    mo_application_ruby_monitoring data
+#    mo_application_ruby_statistics data
   end
 end
 
